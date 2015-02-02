735f5a0eb33153f84d26b58d0d072aab6f6f22c8
Yuriy <sdex@byob.mail.ua>
2015-02-02 11:41:34 +0200

    add base classes

---
+++ app/src/main/java/com/sdex/webteb/activities/BaseActivity.java
@@ -0,0 +1,9 @@
+package com.sdex.webteb.activities;
+
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by Yuriy Mysochenko on 02.02.2015.
+ */
+public class BaseActivity extends ActionBarActivity {
+}
---
+++ app/src/main/java/com/sdex/webteb/fragments/BaseFragment.java
@@ -0,0 +1,9 @@
+package com.sdex.webteb.fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Yuriy Mysochenko on 02.02.2015.
+ */
+public class BaseFragment extends Fragment {
+}
