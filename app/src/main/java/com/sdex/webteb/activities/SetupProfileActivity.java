11469349b86cc6c454bd781e3df938f36d3b24d3
mpodolskiy <matviy.podolsky@gmail.com>
2015-02-02 18:26:04 +0200

    bound login activity with main activity

--- app/src/main/java/com/sdex/webteb/activities/SetupProfileActivity.java
+++ app/src/main/java/com/sdex/webteb/activities/SetupProfileActivity.java
@@ -1,8 +1,11 @@
 package com.sdex.webteb.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.sdex.webteb.R;
+
+import butterknife.OnClick;
 
 /**
  * Created by MPODOLSKY on 02.02.2015.
@@ -17,5 +20,12 @@ public class SetupProfileActivity extends BaseActivity {
     @Override
     protected int getLayoutResource() {
         return R.layout.activity_setup_profile;
+    }
+
+    @OnClick(R.id.done)
+    public void launchMainActivity() {
+        Intent intent = new Intent(SetupProfileActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
