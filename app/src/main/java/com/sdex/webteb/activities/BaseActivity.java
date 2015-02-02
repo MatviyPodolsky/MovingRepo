a7ab9025a186ae0673db5f2891c8cdc80e6ecdbe
Yuriy <sdex@byob.mail.ua>
2015-02-02 11:43:42 +0200

    add base classes

--- app/src/main/java/com/sdex/webteb/activities/BaseActivity.java
+++ app/src/main/java/com/sdex/webteb/activities/BaseActivity.java
@@ -1,9 +1,51 @@
 package com.sdex.webteb.activities;
 
+import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 /**
  * Created by Yuriy Mysochenko on 02.02.2015.
  */
-public class BaseActivity extends ActionBarActivity {
+public abstract class BaseActivity extends ActionBarActivity {
+
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutResource());
+        //toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setTitle(null);
+        }
+    }
+
+    protected abstract int getLayoutResource();
+
+    protected void setActionBarIcon(int iconRes) {
+        toolbar.setNavigationIcon(iconRes);
+    }
+
+    protected void setActionBarTitle(int titleRes) {
+        toolbar.setTitle(titleRes);
+    }
+
+    protected void setActionBarTitle(String title) {
+        toolbar.setTitle(title);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
---
+++ app/src/main/java/com/sdex/webteb/activities/MainActivity.java
@@ -0,0 +1,11 @@
+package com.sdex.webteb.activities;
+
+/**
+ * Created by Yuriy Mysochenko on 02.02.2015.
+ */
+public class MainActivity extends BaseActivity {
+    @Override
+    protected int getLayoutResource() {
+        return 0;
+    }
+}
