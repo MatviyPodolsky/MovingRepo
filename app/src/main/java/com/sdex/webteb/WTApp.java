b55e8fd9dbe064148c5c634318f9d8219a371a33
Yuriy <sdex@byob.mail.ua>
2015-02-03 17:34:56 +0200

    implementing my album screen

--- app/src/main/java/com/sdex/webteb/WTApp.java
+++ app/src/main/java/com/sdex/webteb/WTApp.java
@@ -2,14 +2,6 @@ package com.sdex.webteb;
 
 import android.app.Application;
 
-import com.nostra13.universalimageloader.cache.disc.impl.LimitedAgeDiscCache;
-import com.nostra13.universalimageloader.cache.memory.impl.WeakMemoryCache;
-import com.nostra13.universalimageloader.core.DisplayImageOptions;
-import com.nostra13.universalimageloader.core.ImageLoader;
-import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
-
-import java.io.File;
-
 /**
  * Created by Yuriy Mysochenko on 03.02.2015.
  */
@@ -18,21 +10,6 @@ public class WTApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        initImageLoader();
     }
 
-    public void initImageLoader() {
-        final DisplayImageOptions defaultDisplayOptions = new DisplayImageOptions.Builder()
-                .cacheOnDisk(true)
-                .build();
-        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(getApplicationContext())
-                .defaultDisplayImageOptions(defaultDisplayOptions)
-                .threadPoolSize(3)
-                .threadPriority(Thread.MIN_PRIORITY + 3)
-                .denyCacheImageMultipleSizesInMemory()
-                .memoryCache(new WeakMemoryCache())
-                .build();
-        ImageLoader.getInstance().init(config);
-    }
-    
 }
