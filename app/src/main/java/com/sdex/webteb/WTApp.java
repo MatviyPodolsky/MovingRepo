5478c4eb78a63f75a60b886497584626638b938d
Yuriy <sdex@byob.mail.ua>
2015-03-19 17:52:25 +0200

    code refactoring

--- app/src/main/java/com/sdex/webteb/WTApp.java
+++ app/src/main/java/com/sdex/webteb/WTApp.java
@@ -13,8 +13,6 @@ public class WTApp extends Application {
     public void onCreate() {
         super.onCreate();
         PreferencesManager.initializeInstance(this);
-
-//        PreferencesManager.getInstance().setTokenData("HfXra0ukOClt-et8lqZL-reSmnPIxFrkE4dB6w-8bmDvy679qG-boN2_WOGlAk5px3zfSs8pUQwwb-IhxqKCj0Brbw1Irvn_KFCy9kImwfTDUSE3uvLVu91mXsWh721Jr8CCboGP9uDCMQo1vYFuEg9ZlM_0CTqCSwHqCSXATK2AKexsNFYkN3fGRVU1NyJt7VvJIJ2QDAhMzSAY1LqHp2MiEagkYTPi5CVrFJ5ZIgl6l6zI71NwEvZlFqMhTMcg24sUGyfo6gTIoMkkl8qY0xJ6Yj-tB3SDY2Np5lsXbAz8SOJITFITgD5LqkP3m4IuIGiBPO5Jc1pdzGrFmFT4l_rrhc4XRL_mAQTMyHhxGcAyoWgEpGmWRJioOIe9s309wp3YoHsfBV_ZU__n5dwToJgacMTIeZxqKY7UaLfDi4cKpI-WseoNvu_gVCyPmcWaJecZ_EFaWV1MLnuE_fzRXcf4ToQ", "Bearer");
     }
 
 }
