0af8d398178af56ad312f1f0d69868d20c503b9d
Mazur <omazrv@gmail.com>
2015-03-27 11:21:23 +0200

    fixed bug "Next" button on the 2nd page of profile should be shown as not enabled (not active) - user must define date."

--- app/src/main/java/com/sdex/webteb/fragments/profile/BirthDateFragment.java
+++ app/src/main/java/com/sdex/webteb/fragments/profile/BirthDateFragment.java
@@ -128,6 +128,11 @@ public class BirthDateFragment extends BaseFragment {
 
     private void setButtonEnabled(boolean isEnabled) {
         mBtnNext.setEnabled(isEnabled);
+        if(isEnabled) {
+            mBtnNext.setBackgroundResource(R.drawable.btn_blue_normal);
+        } else {
+            mBtnNext.setBackgroundResource(R.drawable.btn_blue_disabled);
+        }
     }
 
     @OnClick(R.id.category_1)
