ff347b94394ecfb34a27c813aa6aa4e1e35d93c9
Rubanskiy <rubanskiy.a@gmai.com>
2015-04-08 17:08:17 +0300

    code refactoring.

--- app/src/main/java/com/sdex/webteb/utils/Utils.java
+++ app/src/main/java/com/sdex/webteb/utils/Utils.java
@@ -10,9 +10,6 @@ import android.view.ViewTreeObserver;
 import com.sdex.webteb.R;
 import com.sdex.webteb.fragments.profile.BirthDateFragment;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 /**
  * Created by Yuriy Mysochenko on 11.03.2015.
  */
@@ -77,12 +74,7 @@ public final class Utils {
     }
 
     public static boolean isEmailValid(String email) {
-        String expression = "^[\\w\\.-]{2,}+@([\\w\\-]{2,}+\\.)+[A-Z]{2,}$";
-
-        Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
-        Matcher matcher = pattern.matcher(email);
-
-        return matcher.matches();
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
     }
 
     public static boolean isConnected(Context context) {
